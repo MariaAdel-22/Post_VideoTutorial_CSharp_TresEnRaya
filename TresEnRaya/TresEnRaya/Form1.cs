9977a5b4a9881b0cfe6ddd7272eedb25d221d6b5
MariaAdel-22 <maria.espana1@educa.madrid.org>
2022-02-26 09:12:31 +0100

    Add project files.

---
+++ TresEnRaya/TresEnRaya/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TresEnRaya
+{
+    public partial class Form1 : Form
+    {
+
+        TresEnRaya tresEnRaya = new TresEnRaya();
+        private int[,] matriz = new int[2, 2];
+        private int ganador = -1;
+
+        public Form1()
+        {
+            //Inicializamos la partida
+
+            InitializeComponent();
+            tresEnRaya.InicializarPartida();
+            matriz = tresEnRaya.Matriz;
+
+        }
+
+        private void ComprobarGanador()
+        {
+            //En este array obtenemos los últimos movimientos de la máquina.
+
+            int[] ultMov = tresEnRaya.UltimoMovimientoMaquina;
+
+            //En cada if lo que hago es comprobar si el último movimiento de la máquina concide con el último momento realizado (cuando pulsas), y si es así 
+            //aparecerá el botón con el color rosa y el texto 0 para indicar que ha hecho un movimiento la máquina.
+
+            if (ultMov[0] == 0 && ultMov[1] == 0) {
+
+                button1.Text = "0";
+                button1.BackColor = Color.LightPink;
+            }
+
+            if (ultMov[0] == 0 && ultMov[1] == 1) {
+
+                button2.Text = "0";
+                button2.BackColor = Color.LightPink;
+            }
+
+            if (ultMov[0] == 0 && ultMov[1] == 2) {
+
+                button3.Text = "0";
+                button3.BackColor = Color.LightPink;
+            }
+
+            if (ultMov[0] == 1 && ultMov[1] == 0) {
+
+                button6.Text = "0";
+                button6.BackColor = Color.LightPink;
+            }
+
+            if (ultMov[0] == 1 && ultMov[1] == 1) { 
+
+                button5.Text = "0";
+                button5.BackColor = Color.LightPink;
+            }
+
+            if (ultMov[0] == 1 && ultMov[1] == 2) { 
+
+                button4.Text = "0";
+                button4.BackColor = Color.LightPink;
+            }
+
+            if (ultMov[0] == 2 && ultMov[1] == 0) { 
+
+                button9.Text = "0";
+                button9.BackColor = Color.LightPink;
+            }
+
+            if (ultMov[0] == 2 && ultMov[1] == 1) {
+                
+                button8.Text = "0";
+                button8.BackColor = Color.LightPink;
+            }
+
+            if (ultMov[0] == 2 && ultMov[1] == 2) { 
+
+                button7.Text = "0";
+                button7.BackColor = Color.LightPink;
+            }
+               
+
+            if (ganador == 0) MessageBox.Show("Ganaste!");
+             
+
+            if (ganador == 1) MessageBox.Show("Perdiste...");
+        
+
+            if(ganador==-1 && tresEnRaya.TableroLleno())
+                MessageBox.Show("Empate!"); 
+
+
+        }
+
+
+        private void EventoBotones( int x, int y,Button boton)
+        {
+            if (matriz[x, y] == -1)
+            {
+                tresEnRaya.SeleccionarPosicion(x,y);
+                ganador = tresEnRaya.GanaPartida();
+                ComprobarGanador();
+                boton.Text = "X";
+                boton.BackColor = Color.LightCyan;
+
+            }
+        }
+
+        //En cada evento click de cada botón llamamos a un método al cual le pasamos la posición (esa posición es la que ocupa en una matriz) y el nombre del botón
+        //que ha sido pulsado.
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EventoBotones(0, 0, button1);
+       }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EventoBotones(0, 1, button2);
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            EventoBotones(0, 2, button3);
+   
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            EventoBotones(1, 0, button6);
+     
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            EventoBotones(1, 1, button5);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            EventoBotones(1,2, button4);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            EventoBotones(2, 0, button9);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            EventoBotones(2, 1, button8);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            EventoBotones(2, 2, button7);
+        }
+
+        //Este botón es el que tiene como texto "Nueva Partida" y de lo que encarga es de volver a iniciar la partida dándole un nombre vacío a todos los demás botones
+        //es como que se resetea. Además también lo que hacemos es asignarle a la propiedad Matriz, la posición de los botones (que es una matriz en sí)
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            tresEnRaya = new TresEnRaya();
+            tresEnRaya.InicializarPartida();
+            matriz = tresEnRaya.Matriz;
+            ganador = -1;
+
+            label1.Text = button1.Text = button2.Text = button3.Text = button4.Text = button5.Text = button6.Text = button7.Text = button8.Text = button9.Text = String.Empty;
+
+            button1.BackColor = Color.Gray;
+            button2.BackColor = Color.Gray;
+
+            button3.BackColor = Color.Gray;
+            button4.BackColor = Color.Gray;
+
+            button5.BackColor = Color.Gray;
+            button6.BackColor = Color.Gray;
+
+            button7.BackColor = Color.Gray;
+            button8.BackColor = Color.Gray;
+            button9.BackColor = Color.Gray;
+        }
+    }
+
+}
---
+++ TresEnRaya/TresEnRaya/TresEnRaya.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TresEnRaya
+{
+    class TresEnRaya
+    {
+        //Definimos las posiciones de los botones (como el tablero del juego)
+        private int[,] matriz = new int[3, 3];
+        private int ganador = -1;
+        
+        //Guardamos aquí el último movimiento de nuestra Inteligencia Artificial
+        private int[] ultimoMovimientoMaquina = new int[3];
+
+        //Declaramos propiedades para acceder a ellos desde el Form1
+        public int[,] Matriz { get => matriz; set => matriz = value; }
+        public int Ganador { get => ganador; set => ganador = value; }
+        public int[] UltimoMovimientoMaquina { get => ultimoMovimientoMaquina; set => ultimoMovimientoMaquina = value; }
+
+        //Lo que hacemos es recorrer toda la matriz para asignarle el valor -1, ya cuando se vayan pulsando los botones conforme se juegue la partida irá cambiando
+        //los valores de dicha matriz.
+        public void InicializarPartida()
+        {
+            for (int i = 0; i < matriz.GetLength(0); i++)
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                    matriz[i, j] = -1;
+            Ganador = -1;
+        }
+
+        //En este método lo que hacemos es comprobar que nos pasemos los límites que tiene el tablero del juego.
+        public void SeleccionarPosicion(int x, int y)
+        {
+            if (x >= 0 && x < 3 && y >= 0 && y < 3 && matriz[x, y] == -1 && Ganador == -1)
+            {
+                matriz[x, y] = 0;
+                Ganador = GanaPartida();
+                MueveMaquina();
+            }
+        }
+
+        //Comprobamos si alguna de las combinaciones de la matriz es la ganadora
+        public int GanaPartida()
+        {
+            int aux = -1;
+
+            //Compruebo las combinaciones en diagonal
+            if (matriz[0, 0] != -1 && matriz[0, 0] == matriz[1, 1] && matriz[0, 0] == matriz[2, 2])
+                aux = matriz[0, 0];
+
+            if (matriz[0, 2] != -1 && matriz[0, 2] == matriz[1, 1] && matriz[0, 2] == matriz[2, 0])
+                aux = matriz[0, 2];
+
+            //Compruebo las combinaciones en horizontal y vertical
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                if (matriz[i, 0] != -1 && matriz[i, 0] == matriz[i, 1] && matriz[i, 0] == matriz[i, 2])
+                    aux = matriz[i, 0];
+
+                if (matriz[0, i] != -1 && matriz[0, i] == matriz[1, i] && matriz[0, i] == matriz[2, i])
+                    aux = matriz[0, i];
+            }
+
+
+            return aux;
+        }
+
+        //Recorremos la matriz para comprobar si algún elemento del tablero tiene el valor -1, si no es así significa que el tablero ya está lleno.
+        public bool TableroLleno()
+        {
+            bool tableroCompleto = true;
+            for (int i = 0; i < matriz.GetLength(0); i++)
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                    if (matriz[i, j] == -1)
+                        tableroCompleto = false;
+
+
+            return tableroCompleto;
+        }
+
+        //Comprobamos si se ha terminado el juego(ya sea porque el tablero está lleno o porque alguien ha ganado).
+        public bool FinJuego()
+        {
+            bool fin = false;
+            if (TableroLleno() || GanaPartida() != -1)
+                fin = true;
+
+            return fin;
+
+        }
+
+        //Es un algoritmo con el cual se tiene en cuenta los siguientes puntos:
+        //cuál es el mejor movimiento que aplicará la máquina utilizando la máxima o mínima ventaja. Es la posición que más nos convenga
+        public void MueveMaquina()
+        {
+
+            if (!FinJuego())
+            {
+                int f = 0;
+                int c = 0;
+                int v = Int32.MinValue;
+                int aux;
+
+                for (int i = 0; i < matriz.GetLength(0); i++)
+                    for (int j = 0; j < matriz.GetLength(1); j++)
+                        if (matriz[i, j] == -1)
+                        {
+                            matriz[i, j] = 1;
+                            aux = Minimo();
+                            if (aux > v)
+                            {
+                                v = aux;
+                                f = i;
+                                c = j;
+                            }
+
+                            matriz[i, j] = -1;
+                        }
+                matriz[f, c] = 1;
+                ultimoMovimientoMaquina[0] = f;
+                ultimoMovimientoMaquina[1] = c;
+            }
+        }
+
+
+        private int Maximo()
+        {
+            if (FinJuego())
+            {
+                if (GanaPartida() != -1)
+                    return -1;
+                else
+                    return 0;
+            }
+
+            int v = Int32.MinValue;
+            int aux;
+            for (int i = 0; i < matriz.GetLength(0); i++)
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                    if (matriz[i, j] == -1)
+                    {
+                        matriz[i, j] = 1;
+                        aux = Minimo();
+                        if (aux > v)
+                            v = aux;
+
+                        matriz[i, j] = -1;
+                    }
+
+            return v;
+        }
+
+        private int Minimo()
+        {
+            if (FinJuego())
+            {
+                if (GanaPartida() != -1)
+                    return 1;
+                else
+                    return 0;
+            }
+
+            int v = Int32.MaxValue;
+            int aux;
+            for (int i = 0; i < matriz.GetLength(0); i++)
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                    if (matriz[i, j] == -1)
+                    {
+                        matriz[i, j] = 0;
+                        aux = Maximo();
+                        if (aux < v)
+                            v = aux;
+
+                        matriz[i, j] = -1;
+                    }
+
+            return v;
+        }
+    }
+}
